edb7a72924a4956f754f36c8c89ed8365aa5ad88
Osama shahbaz <oshahbaz74@gmail.com>
2023-09-19 03:38:59 +0500

    mongodb uri added to process env

--- app/libs/dbConnect.js
+++ app/libs/dbConnect.js
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI =
-  'mongodb+srv://oshahbaz74:5VDgxbZjQroAVOZg@natourcluster.wk9acd2.mongodb.net/chatGptDb?retryWrites=true';
+const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
   throw new Error('please define the mongo_db url');
